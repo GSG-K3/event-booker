d9c751920e0b1dca97eed7d663f10a232cd1551f
Sahar <sa.froukh92@hotmail.com>
2020-04-22 21:56:52 +0300

    connect database code #53

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
-if (!process.env.DB_URL) throw new Error('No Database URL!!!');
+const connectionString = process.env.DATABASE_URL;
+
+if (connectionString){ throw new Error('DB connection not found');}
 
 const options = {
-  connectionString : process.env.DB_URL,
-  ssl: true
+  connectionString,
+  ssl: !connectionString.includes('localhost')
+
 };
 
 module.exports = new Pool(options);
