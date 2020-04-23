8ad4831aed3a72010b93c6b573c5386327960271
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-04-23 15:44:26 +0300

    fix guid Error by using uuid-ossp Extension  #65
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,14 +1,18 @@
 const { Pool } = require('pg');
-require('env2')('./config.env');
+
+// require('env2')('./config.env');
+
+require('dotenv').config();
 
 const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString){ throw new Error('DB connection not found');}
+if (!connectionString) {
+  throw new Error('DB connection not found');
+}
 
 const options = {
   connectionString,
-  ssl: !connectionString.includes('localhost')
-
+  ssl: !connectionString.includes('localhost'),
 };
 
 module.exports = new Pool(options);
