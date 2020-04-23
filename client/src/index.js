8fa101d0456de60fa06dbe38632ec3f771e25549
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-04-23 15:27:48 +0300

    Add Theme and Main Grid Container in index , and Add BaseLayout in App.js #66
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { Grid } from '@material-ui/core';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import theme from './Theme/MaterialTheme';
+import './Theme/Css/index.css';
 import * as serviceWorker from './serviceWorker';
 
+console.log(theme());
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MuiThemeProvider theme={theme()}>
+    <React.StrictMode>
+      <Grid container direction="column">
+        <App />
+      </Grid>
+    </React.StrictMode>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
