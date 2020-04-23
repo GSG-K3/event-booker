614a9df2254c67c4405d6540e67e7997d82eb5d1
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-04-23 20:38:19 +0300

    Add Router to User , Admin Layout #73
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- client/src/Components/Layout/Base/AdminLayout.jsx
+++ client/src/Components/Layout/Base/AdminLayout.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './../../Common/Header/Header';
+import { Grid } from '@material-ui/core';
 
 export default class AdminLayout extends Component {
   render() {
     return (
-      <div>
-        <h1>hiiii</h1> <p>I am Admin</p>
-      </div>
+      <Router>
+        <Grid direction="column" container>
+          <Grid item xs={12}>
+            <Header
+              showlogo={true}
+              showMeun={true}
+              showAvatar={false}
+              isAvatarImage={false}
+              srcImage={null}
+              Name="User"
+            />
+          </Grid>
+          <Grid item container>
+            <Grid item xs={false} sm={3} />
+            <Grid item container xs={12} sm={6}>
+              <Switch>
+                <h1>Hi , you are in Admin Palne</h1>
+              </Switch>
+            </Grid>
+            <Grid item xs={false} sm={3} />
+          </Grid>
+        </Grid>
+      </Router>
     );
   }
 }
--- client/src/Components/Layout/Base/UserLayout.jsx
+++ client/src/Components/Layout/Base/UserLayout.jsx
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Home from './../Home';
 import Header from './../../Common/Header/Header';
 import { Grid } from '@material-ui/core';
 export default class UserLayout extends Component {
   render() {
     return (
-      <Grid direction="column" container>
-        <Grid item xs={12}>
-          <Header
-            showlogo={true}
-            showMeun={true}
-            showAvatar={false}
-            isAvatarImage={false}
-            srcImage={null}
-            Name="User"
-          />
-        </Grid>
-        <Grid item container>
-          <Grid item xs={false} sm={3} />
-          <Grid item container xs={12} sm={6}>
-            <Home />
+      <Router>
+        <Grid direction="column" container>
+          <Grid item xs={12}>
+            <Header
+              showlogo={true}
+              showMeun={true}
+              showAvatar={false}
+              isAvatarImage={false}
+              srcImage={null}
+              Name="User"
+            />
           </Grid>
-          <Grid item xs={false} sm={3} />
+          <Grid item container>
+            <Grid item xs={false} sm={3} />
+            <Grid item container xs={12} sm={6}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Home />
+              </Switch>
+            </Grid>
+            <Grid item xs={false} sm={3} />
+          </Grid>
         </Grid>
-      </Grid>
+      </Router>
     );
   }
 }
