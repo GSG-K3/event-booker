30142337309f132efd6b54f3e3625b71a6f4c122
rubasider <rubashka12@gmail.com>
2020-04-30 00:53:49 +0300

    the back end for login page #52

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const getEventById = require('./event/getEventById');
+const getUserByName = require('./user/getUserByName');
 
 router.get('/api/event/:id', getEventById);
+
+router.post('/user/login', getUserByName);
 
 module.exports = router;
