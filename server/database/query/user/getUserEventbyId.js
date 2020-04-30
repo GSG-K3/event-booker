dc79c38fd68c23e99b901e417b7eb17052098164
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-01 01:35:01 +0300

    the code of get user Code of event #87
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

---
+++ server/database/query/user/getUserEventbyId.js
@@ -0,0 +1,17 @@
+const connection = require('./../../connection');
+
+module.exports = (eventId, userId) => {
+  const sql = {
+    text: `SELECT events.title , events.event_date , events.event_time ,events.event_status ,
+                  userEvent.gid , userEvent.code , userEvent.attendance_status ,userEvent.note 
+           FROM 
+           users inner join userEvent on 
+           users.id = userEvent.user_id 
+           inner join events on 
+           userEvent.event_id = events.id
+           WHERE  events.gid=$1 AND users.gid=$2;`,
+    value: [eventId, userId],
+  };
+  console.log(sql);
+  return connection.query(sql.text, sql.value);
+};
