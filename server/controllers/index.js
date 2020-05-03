1dc21adbeb5e38eea9efdf54088adea7588132f4
sara219 <whitenoise6014@gmail.com>
2020-05-03 15:04:20 +0300

    fix error

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const getEventById = require('./event/getEventById');
 const getUserByName = require('./user/getUserByName');
-const { getEventById, getupComingEvent } = require('./event/');
+const { getupComingEvent } = require('./event/');
 
 router.get('/api/event/:id', getEventById);
 router.get('/api/envet/getupComingEvent', getupComingEvent);
