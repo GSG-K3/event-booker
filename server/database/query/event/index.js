53e201bbe29a2d745eec39448c48a1ec52c88986
rubasider <32986134+rubasider@users.noreply.github.com>
2020-05-05 00:31:14 +0300

    Add files via upload
    add the missing files
    cancelPlace.js
    index.js

---
+++ server/database/query/event/index.js
@@ -0,0 +1,13 @@
+const getEventById = require('./getEventById');
+// hello from the other side
+const getupComingEvent = require('./getupComingEvent');
+const postEvent = require('./postEvent');
+const takePlace = require('./takePlace');
+const cancelPlace = require('./cancelPlace');
+module.exports = {
+  getEventById,
+  getupComingEvent,
+  takePlace,
+  cancelPlace,
+  postEvent,
+};
