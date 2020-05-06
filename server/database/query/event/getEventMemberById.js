2b0b8723bdcab4a070cb9ecb27933461a7a9df5d
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-06 18:05:20 +0300

    event member query #63
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

---
+++ server/database/query/event/getEventMemberById.js
@@ -0,0 +1,18 @@
+const connection = require('../../connection');
+
+/*
+get userEvent   for how Enroll in the Event
+*/
+module.exports = (eventId) => {
+  const sql = {
+    text: `SELECT users.user_name userEvent.gid , userEvent.code , userEvent.attendance_status ,userEvent.note 
+           FROM 
+           users inner join userEvent on 
+           users.id = userEvent.user_id 
+           inner join events on 
+           userEvent.event_id = events.id
+           where events.gid = $1;`,
+    value: [eventId],
+  };
+  return connection.query(sql.text, sql.value);
+};
--- server/database/query/event/index.js
+++ server/database/query/event/index.js
@@ -4,9 +4,14 @@ const getupComingEvent = require('./getupComingEvent');
 const takePlace = require('./takePlace');
 const getEventsDay = require('./getEventsDay');
 
+const getEventMemberById = require('./getEventMemberById');
+const getEventMemberInfoById = require('./getEventMemberInfoById');
+
 module.exports = {
   getEventById,
   getupComingEvent,
   takePlace,
   getEventsDay,
+  getEventMemberById,
+  getEventMemberInfoById,
 };
