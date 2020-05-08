66d591d0b63cdb7c8eb47301df9790f37c19418b
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-08 23:40:15 +0300

    add Validation
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -1,10 +1,41 @@
-const { addUser } = require('./../../database/query/user');
-const responsemessage = require('./../../helpers/responseMessage');
+const { addUser } = require('../../database/query/user');
+
+const { InternalErrorMessage } = require('../../helpers/responseMessage');
+
+const { registrationValidation } = require('../../helpers/Validation');
+
 const signup = (req, res) => {
-  const data = req.body;
+  const data = !req.body ? null : req.body;
+
+  if (!data) {
+    return res
+      .status(501)
+      .json(
+        InternalErrorMessage(
+          null,
+          'Sorry Some Error Happened at registration please try again later',
+        ),
+      );
+  }
+
+  const { error } = registrationValidation(data);
+
+  if (error !== undefined) {
+    // return error message if not valid
+
+    const errorMessage = error.toString().includes('[ref:password]')
+      ? 'the password not match , please re-Enter password'
+      : error.toString().replace('ValidationError:', '');
+
+    return res
+      .status(400)
+      .json(responseMessages.FailedMessage(null, `Oops ! ${errorMessage}`));
+  }
+
   addUser(data, (err, result) => {
     console.log('err' + err);
     console.log('result' + result);
+
     if (err) {
       return;
       res
