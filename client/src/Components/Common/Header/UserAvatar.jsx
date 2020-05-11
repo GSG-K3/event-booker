434c1e5c656ca813d1e98e23716ce692e3edc8c5
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-11 21:07:31 +0300

    add style
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- client/src/Components/Common/Header/UserAvatar.jsx
+++ client/src/Components/Common/Header/UserAvatar.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 
+import clsx from 'clsx';
 const useStyles = makeStyles((theme) => ({
   large: {
     backgroundColor: theme.palette.secondary.main,
     width: theme.spacing(7),
     height: theme.spacing(7),
   },
+  margin: { margin: 15 },
 }));
 
-export default function UserAvatar({ showAvatar, isAvatarImage, srcImage, Name, cssClass }) {
+export default function UserAvatar({
+  showAvatar,
+  isAvatarImage,
+  srcImage,
+  Name,
+  cssClass,
+}) {
   const classes = useStyles();
   if (!showAvatar) return '';
-
-  console.log('cssClass', cssClass);
   const css = !cssClass ? classes.large : cssClass;
-  console.log('css', css);
-  const avatar = isAvatarImage ? <Avatar alt={Name} src={srcImage} className={css} /> : <Avatar className={css}>{GetAvatarName(Name)}</Avatar>;
+  const avatar = isAvatarImage ? (
+    <Avatar alt={Name} src={srcImage} className={clsx(css, classes.margin)} />
+  ) : (
+    <Avatar className={clsx(css, classes.margin)}>{GetAvatarName(Name)}</Avatar>
+  );
 
   return avatar;
 }
