6de1ee2df2971257b35cacc446ff20d0f7885411
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-12 05:03:01 +0300

    update query to use of multi column
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- server/database/query/event/updateEventMemberCount.js
+++ server/database/query/event/updateEventMemberCount.js
@@ -1,8 +1,13 @@
 const connection = require('../../connection');
 
-module.exports = (eventId, count) => {
+// update attendance_cnt , member_cnt
+// if isAttendance === true then we need to update the attendance_cnt,
+// otherWise update 'member_cnt'
+module.exports = (eventId, count, isAttendance) => {
+  const columeName = isAttendance ? 'attendance_cnt' : 'member_cnt';
+
   const sql = {
-    text: 'UPDATE events SET  member_cnt = $1 where gid = $2',
+    text: `UPDATE events SET  ${columeName} = $1 where gid = $2`,
     value: [count, eventId],
   };
   return connection.query(sql.text, sql.value);
