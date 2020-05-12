ba12d508d6b7ec4e2c6f140941686c55548822db
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-12 06:27:15 +0300

    reCode from Promise  to await
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- server/controllers/admin/event/takeMemberCode.js
+++ server/controllers/admin/event/takeMemberCode.js
@@ -10,71 +10,72 @@ const {
   FailedMessage,
 } = require('../../../helpers/responseMessage');
 
-module.exports = (req, res) => {
-  let userEventId = 0;
-  let count = 0;
+module.exports = async (req, res) => {
   const { eventId, userId, code } = req.body;
-  checkUserCode(eventId, userId, code)
-    .then((result) => {
-      if (result.rowCount === 0) {
-        return res
-          .status(400)
-          .json(FailedMessage(null, 'the Code is not Valid'));
-      }
-      return result.rows[0].id;
-    })
-    .then((id) => {
-      // id of user Event
-      // update Status Code
-      // to uesing if any error hapend we event update
-      userEventId = id;
-      // 1 means true
-      return updateStatusCode(userEventId, 1);
-    })
-    .then((updateResult) => {
-      // status code not updated
-      if (updateResult.rowCount === 0) {
-        return res
-          .status(400)
-          .json(FailedMessage(null, 'the Code is not Valid'));
-      }
 
-      // status code  updated and must
-      // get event to update attendance cnt
-      return getEventById(eventId);
-    })
-    .then(async (event) => {
-      // event not found so we must reject the code
-      if (event.rowCount === 0) {
-        // set status code to false
-        // 0 means false
-        await updateStatusCode(userEventId, 0);
-        return res.status(400).json(FailedMessage(null, 'Error at Get Event'));
-      }
-      // the event is found
-      // update attendance cnt in event
-      // set isAttendance = true to update  attendance_cnt column
-      count = event.rows[0].attendance_cnt + 1;
-      return updateEventMemberCount(eventId, count, true);
-    })
-    .then(async (updateEventMemberCounts) => {
-      // updat Event Member Count  Error  so we must reject the code
-      if (updateEventMemberCounts.rowCount === 0) {
-        // set status code to false
-        // 0 means false
-        await updateStatusCode(userEventId, 0);
-        return res
-          .status(400)
-          .json(FailedMessage(null, 'Error at update Event'));
-      }
+  try {
+    const checkCode = await checkUserCode(eventId, userId, code);
+
+    // the code is not Valid of this event and this user
+    if (checkCode.rowCount === 0) {
+      res.status(400).json(FailedMessage(null, 'the Code is not Valid'));
+      return;
+    }
+
+    // check if the code used before
+    if (checkCode.rows[0].attendance_status === true) {
       res
-        .status(200)
-        .json(successMessage({ count, userId }, 'The Code is Valid'));
-    })
-    .catch((err) => {
-      console.log(err);
-      return res
-        .status(501)
-        .json(InternalErrorMessage(null, 'internal error with the server'));
-    });
+        .status(400)
+        .json(FailedMessage(null, 'this Code is not Valid , is already used '));
+      return;
+    }
+
+    // id of user Event
+    const userEventId = checkCode.rows[0].id;
+
+    // update Status Code
+    const updateResult = await updateStatusCode(userEventId, true);
+
+    // status code not updated
+    if (updateResult.rowCount === 0) {
+      res
+        .status(400)
+        .json(FailedMessage(null, 'Error at update Status User Code'));
+      return;
+    }
+
+    // status code  updated and must
+    // get event to update attendance count
+    const event = await getEventById(eventId);
+
+    // event not found so we must reject the code
+    if (event.rowCount === 0) {
+      // set status code to false to reject
+      const rejectSatus = await updateStatusCode(userEventId, false);
+      res.status(400).json(FailedMessage(null, 'Error at Get Event'));
+      return;
+    }
+    // the event is found
+    // update attendance count in event
+    // set isAttendance = true to update  attendance_cnt column
+    const count = event.rows[0].attendance_cnt + 1;
+    const updateCount = await updateEventMemberCount(eventId, count, true);
+
+    // updat Event Member Count Error  so we must reject the code
+    if (updateCount.rowCount === 0) {
+      // set status code to false to reject
+      updateStatusCode(userEventId, false);
+      res.status(400).json(FailedMessage(null, 'Error at update Event'));
+      return;
+    }
+
+    res
+      .status(200)
+      .json(successMessage({ count, userId }, 'The Code is Valid'));
+  } catch (err) {
+    console.log('Error in Take Member : ', err);
+    res
+      .status(501)
+      .json(InternalErrorMessage(null, 'internal error with the server'));
+  }
 };
