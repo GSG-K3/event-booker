6aaa842667e2db918209463e488dc6d650c901e1
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-12 11:11:44 +0300

    code to check email
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -2,5 +2,6 @@ const login = require('./login');
 const profile = require('./profile');
 const signup = require('./signup');
 const userEvent = require('./userEvent');
+const checkUserEmail = require('./checkUserEmail');
 
-module.exports = { login, profile, signup, userEvent };
+module.exports = { login, profile, signup, userEvent, checkUserEmail };
--- server/database/query/user/index.js
+++ server/database/query/user/index.js
@@ -3,6 +3,7 @@ const getAllUserEvent = require('./getAllUserEvent');
 const getUserById = require('./getUserById');
 const deleteUserEventbyId = require('./deleteUserEventbyId');
 const addUser = require('./addUser');
+const getUserByEmail = require('./getUserByEmail');
 
 module.exports = {
   getUserById,
@@ -10,4 +11,5 @@ module.exports = {
   getUserEventById,
   deleteUserEventbyId,
   addUser,
+  getUserByEmail,
 };
