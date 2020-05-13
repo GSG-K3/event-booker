4ac2328295289f985eb638bdf6e8ece1ea20ce1a
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2020-05-13 23:58:00 +0300

    Update EventContainer.jsx

--- client/src/Components/Common/Event/EventContainer.jsx
+++ client/src/Components/Common/Event/EventContainer.jsx
@@ -79,7 +79,7 @@ class EventContainer extends Component {
           index={index}
           key={item.id.toString()}
         >
-          <Grid container>{eventCard}</Grid>
+          <Grid container justify="center">{eventCard}</Grid>
         </EventCardContainer>,
       );
       if (index === eventData.length - 1) {
