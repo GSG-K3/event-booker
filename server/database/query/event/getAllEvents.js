669f1f6d4cd192b36285eb61fd2fe814af3ec681
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-05-14 23:10:09 +0300

    add order by
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- server/database/query/event/getAllEvents.js
+++ server/database/query/event/getAllEvents.js
@@ -2,7 +2,7 @@ const connection = require('../../connection');
 
 module.exports = () => {
   const sql = {
-    text: 'select * from events',
+    text: 'select * from events order by event_date DESC , event_time ASC;',
   };
   return connection.query(sql.text, sql.value);
 };
