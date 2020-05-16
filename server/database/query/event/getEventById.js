64267e758adad0bc3c5cbb709ebb593d89c67e0b
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2020-05-16 14:54:19 +0300

    Update getEventById.js

--- server/database/query/event/getEventById.js
+++ server/database/query/event/getEventById.js
@@ -3,7 +3,7 @@ const connection = require('../../connection');
 const getEventById = (id) => {
   const sql = {
     text:
-      'SELECT gid,title,category_id,description,event_date,event_time,event_location,event_status,host,member_cnt ,attendance_cnt FROM events where gid = $1;',
+      'SELECT id,gid,title,category_id,description,event_date,event_time,event_location,event_status,host,member_cnt ,attendance_cnt FROM events where gid = $1;',
     values: [id],
   };
   return connection.query(sql.text, sql.values);
