6b41ff2bcbf3e1a875945d66e2ffad44ad6b1142
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-06-22 14:40:51 +0300

    remove console log

--- client/src/Components/Layout/Profile/Settings.jsx
+++ client/src/Components/Layout/Profile/Settings.jsx
@@ -2,12 +2,9 @@ import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 import ChangePassword from './SettingComponent/ChangePassword';
 export default ({ userInfo, setIsLoading }) => {
-  console.log(userInfo);
   return (
     <Grid container>
-      {/* <h1>{{ ...userInfo }}</h1> */}
-      {/* <h1>sdfsd</h1> */}
-      <ChangePassword userInfo={userInfo} setIsLoading={setIsLoading} />
+      <ChangePassword setIsLoading={setIsLoading} />
     </Grid>
   );
 };
